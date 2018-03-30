5b5c5e79c1b56946a5e8723e855a999328a9310b
David Goeth <goeth@fim.uni-passau.de>
2018-03-30 21:20:12 +0200

    javadocs for image_wrapper config mapping

--- implementation/ImageWrapper/app/de/unipassau/medspace/wrapper/image_wrapper/config/mapping/DDSM_MappingConfigReader.java
+++ implementation/ImageWrapper/app/de/unipassau/medspace/wrapper/image_wrapper/config/mapping/DDSM_MappingConfigReader.java
@@ -13,15 +13,15 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * TODO
+ * A reader for the DDSM maping configuration file.
  */
 public class DDSM_MappingConfigReader {
 
   private final String mappingSpecificationSchema;
 
   /**
-   * TODO
-   * @param mappingSpecificationSchema
+   * Creates a new DDSM_MappingConfigReader.
+   * @param mappingSpecificationSchema The validation XSD file to use.
    */
   public DDSM_MappingConfigReader(String mappingSpecificationSchema) {
     this.mappingSpecificationSchema = mappingSpecificationSchema;
@@ -29,11 +29,11 @@ public class DDSM_MappingConfigReader {
 
 
   /**
-   * TODO
-   * @param fileName
-   * @throws JAXBException
-   * @throws IOException
-   * @throws SAXException
+   * Parses a DDSM maping configuration file.
+   * @param fileName The DDSM maping configuration file.
+   * @throws JAXBException If an error occurs regarding JAXB-
+   * @throws IOException If an IO error occurs.
+   * @throws SAXException If an XML error occurs.
    */
   public RootMapping parse(String fileName) throws JAXBException, IOException, SAXException {
     JAXBContext context = JAXBContext.newInstance(RootMapping.class);
