d3069f8887a74720f973fe78cbd85c24032e98e6
David Goeth <goeth@fim.uni-passau.de>
2018-03-31 15:34:04 +0200

    Javadocs for Query

--- implementation/medspace/app/de/unipassau/medspace/query_executor/Query.java
+++ implementation/medspace/app/de/unipassau/medspace/query_executor/Query.java
@@ -3,7 +3,7 @@ package de.unipassau.medspace.query_executor;
 import de.unipassau.medspace.common.register.Service;
 
 /**
- * A dummy class for a base Query class
+ * Represents a query.
  */
 public class Query {
 
@@ -11,15 +11,28 @@ public class Query {
 
   private final String queryString;
 
+  /**
+   * Creates a Query new object.
+   * @param service The service to call.
+   * @param queryString The query.
+   */
   public Query(Service service, String queryString) {
     this.service = service;
     this.queryString = queryString;
   }
 
+  /**
+   * Provides the service.
+   * @return the service.
+   */
   public Service getService() {
     return service;
   }
 
+  /**
+   * Provides the query.
+   * @return the query.
+   */
   public String getQueryString() {
     return queryString;
   }
